1f0d27363427ce550c4da581bfcd35cda2b0dec3
User <User@DESKTOP-141UIPT>
2022-03-17 10:33:54 +0530

    THIRD COMMIT

---
+++ src/test/java/Project/SampleProject2ForParameterizationTest.java
@@ -0,0 +1,19 @@
+package Project;
+
+import org.testng.annotations.Test;
+
+public class SampleProject2ForParameterizationTest {
+	
+@Test
+	
+	public void runInGit()
+	{
+		
+		String URL= System.getProperty("url");
+		System.out.println(URL);
+		
+	}
+			
+
+
+}
